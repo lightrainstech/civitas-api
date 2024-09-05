6181c3396386756556680455b0b51f9711b50616
niksmac <niksmac@lightrains.com>
2024-09-05 14:55:23 +0530

    Add proj list

--- src/services/v1/public.js
+++ src/services/v1/public.js
@@ -1,8 +1,21 @@
 'use strict'
+const Project = require('@models/projectModel.js')
 
 module.exports = async function (fastify, opts) {
-  fastify.get('/projects/list', {}, async function (request, reply) {
+  fastify.post('/projects/list', {}, async function (request, reply) {
+    const { searchTerm, chain, category, page } = request.body
+    const projectModel = new Project()
     try {
+      const projects = await projectModel.getProjects(
+        searchTerm,
+        chain,
+        category,
+        page
+      )
+      reply.success({
+        message: 'Project created successfully',
+        data: projects
+      })
     } catch (error) {
       console.log(error)
     }
