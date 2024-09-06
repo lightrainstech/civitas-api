6d852d33501ca6dfadf6ab0d1308c71512a280f9
sruthi23 <sruthi@lightrains.com>
2024-09-06 11:23:20 +0530

    Fix:structure of args

--- src/services/v1/public.js
+++ src/services/v1/public.js
@@ -6,12 +6,12 @@ module.exports = async function (fastify, opts) {
     const { searchTerm, chain, category, page } = request.body
     const projectModel = new Project()
     try {
-      const projects = await projectModel.getProjects(
+      const projects = await projectModel.getProjects({
         searchTerm,
         chain,
         category,
         page
-      )
+      })
       reply.success({
         message: 'Project created successfully',
         data: projects
@@ -20,5 +20,37 @@ module.exports = async function (fastify, opts) {
       console.log(error)
     }
     return reply
-  })
+  }),
+    fastify.post(
+      '/projects/image/upload/:fileType',
+      {},
+      async function (request, reply) {
+        try {
+          let { fileType } = request.params
+          let formData = await request.file(),
+            mimeType = formData.mimetype,
+            fileName = formData.filename.replace(/[^a-zA-Z0-9.]/g, ''),
+            filePath = `${process.cwd()}/public/images/${Date.now()}${fileName}`
+          pipeline(formData.file, fs.createWriteStream(filePath), async err => {
+            if (err) {
+              reply.error(err)
+            }
+            let imageUrl = await saveToS3(
+              filePath,
+              `${Date.now()}-${fileName}`,
+              fileType
+            )
+            fs.unlinkSync(filePath)
+            reply.success({
+              message: 'Image uploaded successfully.',
+              path: imageUrl.Location,
+              mimeType: mimeType
+            })
+          })
+        } catch (err) {
+          reply.error(err)
+        }
+        return reply
+      }
+    )
 }
