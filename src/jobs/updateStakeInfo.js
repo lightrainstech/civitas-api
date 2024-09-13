a62f2b0b0267ce0fbddbc298d0e3776d4cf50dff
sruthi23 <sruthi@lightrains.com>
2024-09-13 09:37:04 +0530

    Update:fetch valuts

--- src/jobs/updateStakeInfo.js
+++ src/jobs/updateStakeInfo.js
@@ -1,8 +1,26 @@
+'use strict'
+const Project = require('@models/projectModel.js')
+const { fetchTotalStake } = require('../utils/contractUtils.js')
 module.exports = async function (agenda) {
   agenda.define('Update:StakeInfo', async (job, done) => {
     try {
-      console.log('here')
-      job.remove()
+      const projectModel = new Project()
+      const projects = await projectModel.getProjectWithoutPagination()
+      for await (const project of projects) {
+        let chain = project?.chain || null,
+          valuts = projectModel?.vault || []
+        if (vault.length > 0) {
+          for await (const vault of valuts) {
+            if (chain && vault?.status === 'active') {
+              let stakes = await fetchTotalStake({
+                valutAddress: vault.vaultAddress,
+                chain
+              })
+              //update stakes
+            }
+          }
+        }
+      }
       done()
     } catch (e) {
       console.log('--------ERROR UPDATING STAKING INFO--------')
